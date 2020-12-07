22469b97374874405e1c5312189c9774d066e24c
ntwari-egide <ntwariegide2@gmail.com>
2020-12-07 11:15:34 +0200

    Configured every required details for advanced pdf writer

--- src/main/java/com/springboot/pdf/controller/AdvancedPdfWriterController.java
+++ src/main/java/com/springboot/pdf/controller/AdvancedPdfWriterController.java
@@ -1,4 +1,17 @@
 package com.springboot.pdf.controller;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileOutputStream;
+
+@RestController
 public class AdvancedPdfWriterController {
+    @GetMapping("/advancedpdf1")
+    public Document creatingAdvancedPdf()throws Exception{
+        Document document = new Document();
+        PdfWriter.getInstance(document,new FileOutputStream("advancedPdf1.pdf"));
+    }
 }
