4e6fac9f4a9ad9fc0cfdd258a81843574eaf8d3a
ntwari-egide <ntwariegide2@gmail.com>
2020-12-07 09:40:43 +0200

    Adding controller function for get request

--- src/main/java/com/springboot/pdf/controller/PdfManagerController.java
+++ src/main/java/com/springboot/pdf/controller/PdfManagerController.java
@@ -1,8 +1,13 @@
 package com.springboot.pdf.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PdfManagerController {
 
+    @GetMapping("/make-pdf/itext")
+    public String creatingPdfUsingIText(){
+        return "File pf is being created ";
+    }
 }
