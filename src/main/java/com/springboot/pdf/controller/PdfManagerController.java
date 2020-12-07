e822869c5588707c4a17b28e15e25a16f4a9a4d8
ntwari-egide <ntwariegide2@gmail.com>
2020-12-07 09:44:21 +0200

    Added pdf writer get instance

--- src/main/java/com/springboot/pdf/controller/PdfManagerController.java
+++ src/main/java/com/springboot/pdf/controller/PdfManagerController.java
@@ -1,13 +1,21 @@
 package com.springboot.pdf.controller;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileOutputStream;
 
 @RestController
 public class PdfManagerController {
 
     @GetMapping("/make-pdf/itext")
-    public String creatingPdfUsingIText(){
-        return "File pf is being created ";
+    public void creatingPdfUsingIText()throws Exception{
+        Document document = new Document();
+
+        // creating the instance of the PdfWriter class
+        PdfWriter.getInstance(document,new FileOutputStream("MycreatedPdf.pdf"));
+//        return "File pf is being created ";
     }
 }
