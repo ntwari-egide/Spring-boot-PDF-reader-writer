fe4a8a813bdadbba79e375998aec5f07e7b73a41
ntwari-egide <ntwariegide2@gmail.com>
2020-12-07 09:31:00 +0200

    Initial commit for the project

---
+++ src/main/java/com/springboot/pdf/PdfWritterAndReaderSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfWritterAndReaderSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfWritterAndReaderSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/pdf/PdfWritterAndReaderSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfWritterAndReaderSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
